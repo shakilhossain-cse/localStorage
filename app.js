88782187142ffa79b14bfeb69ca1b109b7801ab5
Shakil Hossain <shakilhossainp.cse@gmail.com>
2021-09-09 14:02:18 +0600

    store and delate all item

--- app.js
+++ app.js
@@ -1,48 +1,23 @@
-/* Task 1 
-setTimeout(()=>{
-    console.log('hello friends ');
-}, 3500);
-Task 2
-const number = prompt('type anything');
-const num = parseInt(number);
-alert(num + 200);
-Task 3
-const confirmMsg = confirm('do you want to see your location??');
-if (confirmMsg) {
-    console.log(location.href);
-}
-Task 4
-What is cookiez 
-cookies are text file with data - like a username and password - that are used to identify your computer as you use a computer network . specific cookies known as http cookies are use to identify specific users and improve your web experience
-Data stored in a cookie is created by the server upon your connection . this data is labeled with an Id unique to you and your computer.
-Task 5
-diffrent between localStorage and sessionStorage 
-Local Storage
-
-Has no expiration date
-Client only
-Has no SSL support
-Data are not transferred on each HTTP request
-5 mb limit (check with the browser)
-Session Storage
-
-Data is gone when you close the browser tab
-Client only
-Has no SSL support
-Data are not transferred on each HTTP request
- 5-10 mb limit (check with the browser) */
-
 
  const input = document.getElementById('input');
  const button = document.getElementById('button');
  const ul = document.getElementById('ul');
+ const clearAll = document.getElementById('clear');
+
+const displayTodoItems = () => {
+    const todoItem = getAllTodo();
+    for (const item in todoItem) {
+        displayItem(item)
+    }
+}
+
 
  button.addEventListener('click',()=>{
      if (!input.value) {
          return;
      }
     //  display add item 
-    displayItem()
+    displayItem(input.value)
 
     // add item on localStorage 
     addItemsTodo(input.value)
@@ -53,10 +28,10 @@ Data are not transferred on each HTTP request
 
 
 
-const displayItem = ()=>{
+const displayItem = value =>{
     const div = document.createElement('div');
     div.classList.add('row')
-    div.innerHTML= `<p>${input.value}</p>
+    div.innerHTML= `<p>${value}</p>
                 <div class='icon'>
                 <i class="fas fa-times"></i>
                 </div> 
@@ -77,10 +52,13 @@ const getAllTodo = () => {
 
 const addItemsTodo = item =>{
     const stodo = getAllTodo();
-    stodo[item] = 1;
+    if (stodo[item]) {
+        stodo[item] = stodo[item]+1
+    }else{
+        stodo[item] = 1;
+    }
     const itemStringify = JSON.stringify(stodo)
-    localStorage.setItem('todo',itemStringify)
-    console.log(itemStringify);   
+    localStorage.setItem('todo',itemStringify)   
 }
 
 
@@ -92,8 +70,12 @@ ul.addEventListener('click',(e)=>{
     }
 })
 
+clearAll.addEventListener('click',()=>{
+    ul.textContent = '';
+    localStorage.removeItem('todo');
+});
 
-
+displayTodoItems();
 
 
 
